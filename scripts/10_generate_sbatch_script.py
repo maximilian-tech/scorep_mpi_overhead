43f1f404d3fe2768d8bfbe0df5361a26e02b2f9b
Maximilian Sander <maximilian.sander1@tu-dresden.de>
2025-07-02 00:27:49 +0900

    Measure Score-P Overhead within the same allocation

--- scripts/10_generate_sbatch_script.py
+++ scripts/10_generate_sbatch_script.py
@@ -4,8 +4,10 @@ import sys
 
 script_path = Path(__file__).resolve()
 
-ncores = [4,8,16,32,64,96]
-nnodes = [1,4]
+#ncores = [4,8,16,32,64,96]
+#nnodes = [1,4]
+ncores = [4,96]
+nnodes = [1]
 
 root_dir: Path      = Path(script_path).parent.parent
 tools_dir: Path     = root_dir / "tools"
@@ -23,25 +25,33 @@ for toolchain in toolchains:
 
     scorep_bin_dir: Path = tools_dir / ("scorep_" + toolchain_name ) / "bin"
 
-    for scorep_enabled in ['scorepOFF', 'scorepON']:
-        osu_dir: Path = tools_dir / f"osu_{toolchain_name}_{scorep_enabled}" / \
-                                    "libexec" / \
-                                    "osu-micro-benchmarks" / \
-                                    "mpi"
+    #for scorep_enabled in ['scorepOFF', 'scorepON']:
+    if True:
+        # osu_dir: Path = tools_dir / f"osu_{toolchain_name}_{scorep_enabled}" / \
+        #                             "libexec" / \
+        #                             "osu-micro-benchmarks" / \
+        #                             "mpi"
 
         for nodes in nnodes:
             for cores in ncores:
-                append = root_dir / "results" / f"{toolchain_name}_{scorep_enabled}_n{nodes:03d}_c{cores:05d}"
-                scorep_experiment_dir = root_dir / "scorep_results" / f"{toolchain_name}_{scorep_enabled}_n{nodes:03d}_c{cores:05d}"
+                #
+                #scorep_experiment_dir = root_dir / "scorep_results" / f"{toolchain_name}_{scorep_enabled}_n{nodes:03d}_c{cores:05d}"
 
-                rm_old_output_cmds = "\n".join(f"rm -f {append}_{exe.replace('/','-')}" for exe in executables)
+                common_part = f"{toolchain_name}_${{scorep_enabled}}_n{nodes:03d}_c{cores:05d}"
+                name = common_part.replace(r"_${scorep_enabled}_","_")
+                print(name)
+                append = f"{root_dir}/results/{common_part}"
+                rm_old_output_cmd = "\n".join(f"\trm -f {append}_{exe.replace('/','-')}" for exe in executables)
 
-                run_cmds = "\n".join(f"\tsrun ./{exe} -z >> {append}_{exe.replace('/','-')}" for exe in executables)
+                append = f"{root_dir}/results/{common_part}"
+                run_cmds = "\n".join(f'\t\tsrun ./{exe} -z >> "{append}_{exe.replace("/","-")}"' for exe in executables)
+
+                osu_dir = f"{tools_dir}/osu_{toolchain_name}_${{scorep_enabled}}/libexec/osu-micro-benchmarks/mpi"
 
                 sbatch_script = f"""
 #!/bin/bash
 
-#SBATCH -J {append.name}
+#SBATCH -J {name}
 #SBATCH -N {nodes}
 #SBATCH -n {cores}
 #SBATCH --switch=1
@@ -50,25 +60,32 @@ for toolchain in toolchains:
 #SBATCH --constraint=no_monitoring
 #SBATCH --hint=nomultithread
 #SBATCH --mem=0
+#SBATCH --output="{root_dir}/jobs/{name}.out"
 #SBATCH -x n1609,n1016
 
 source {toolchain}
 export PATH={scorep_bin_dir}:$PATH
-export SCOREP_EXPERIMENT_DIRECTORY={scorep_experiment_dir}
 export SCOREP_ENABLE_PROFILING=True
 export SCOREP_ENABLE_TRACING=True
 
-mkdir -p "${{SCOREP_EXPERIMENT_DIRECTORY%/*}}"
+for scorep_enabled in scorepOFF scorepON ; do
+{rm_old_output_cmd}
+done
 
-cd {osu_dir}
-
-{rm_old_output_cmds}
+mkdir -p {root_dir}/scorep_results
 
 for i in {{1..10}} ; do
+  for scorep_enabled in scorepOFF scorepON ; do
+
+    cd "{osu_dir}"
+    export SCOREP_EXPERIMENT_DIRECTORY="{root_dir}/scorep_results/{common_part}"
+ 
 {run_cmds}
+    
+  done
 done
 """
-                out = root_dir / "jobs" / f"{append.name}.sh"
+                out = root_dir / "jobs" / f"{name}.sh"
                 out.parent.mkdir(exist_ok=True, parents=True)
                 out.write_text(sbatch_script.lstrip())
   
